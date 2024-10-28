2e4292fd2748aaeb8534f9537a5af5e6f6d3bcc3
FederCO <federico.bessi@gmail.com>
2024-10-28 12:43:32 -0300

    extract images script new version - correct file names

--- 5-Data_Wrangling/extractImages_script.py
+++ 5-Data_Wrangling/extractImages_script.py
@@ -9,12 +9,12 @@ import pystac_client
 if __name__ == '__main__':
     # Define the global variables for the captures
     service = pystac_client.Client.open('https://data.inpe.br/bdc/stac/v1/')  # using Brazil Data Cube as data source
-    center_point = (-30.135095, -51.382719)  # PV plant coordinates
-    place_name = "Guaiba"
+    center_point = (-8.041142, -35.012587)  # PV plant coordinates
+    place_name = "SãoLorenço"
     output_path = ".\\data\\"
     nb_images = 20  # number of captures
     offset_distance_km = 1.5  # Define the distance to extend from the center_point for the main bounding box
-    date_interval = '2024-07-01/2024-08-31'  # date interval for cloud treatment
+    date_interval = '2023-11-01/2023-12-31'  # date interval for cloud treatment
 
     captureWindow(service,
               center_point,
